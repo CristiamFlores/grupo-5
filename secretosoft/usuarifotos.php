f0e6f04a20ca9659bd7c8ce50b91d0c706e9e16e
EDWIN ROJAS <103547525+edwinrojas99@users.noreply.github.com>
2022-04-27 20:41:49 -0400

    Add files via upload

---
+++ secretosoft/usuarifotos.php
@@ -0,0 +1,8 @@
+<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                            <label>Imagen:</label>
+                            <input type="file" class="form-control" name="imagen" id="imagen">
+                            <input type="hidden" name="imagenactual" id="imagenactual">
+                            <img src="" width="150px" height="120px" id="imagenmuestra">
+                          </div>
+                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
