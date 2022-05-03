e27b0eb0f2a266e153d753a53e999c3f3275a2f8
CristiamFlores <103546758+CristiamFlores@users.noreply.github.com>
2022-05-02 22:04:24 -0400

    Add files via upload

---
+++ script  fechas.php
@@ -0,0 +1,65 @@
+<?php 
+//Incluímos inicialmente la conexión a la base de datos
+require "../config/Conexion.php";
+
+Class Consultas
+{
+	//Implementamos nuestro constructor
+	public function __construct()
+	{
+
+	}
+
+	public function comprasfecha($fecha_inicio,$fecha_fin)
+	{
+		$sql="SELECT DATE(i.fecha_hora) as fecha,u.nombre as usuario, p.nombre as proveedor,i.tipo_comprobante,i.serie_comprobante,i.num_comprobante,i.total_compra,i.impuesto,i.estado FROM ingreso i INNER JOIN persona p ON i.idproveedor=p.idpersona INNER JOIN usuario u ON i.idusuario=u.idusuario WHERE DATE(i.fecha_hora)>='$fecha_inicio' AND DATE(i.fecha_hora)<='$fecha_fin'";
+		return ejecutarConsulta($sql);		
+	}
+
+	public function ventasfechacliente($fecha_inicio,$fecha_fin,$idcliente)
+	{
+		$sql="SELECT DATE(v.fecha_hora) as fecha,u.nombre as usuario, p.nombre as cliente,v.tipo_comprobante,v.serie_comprobante,v.num_comprobante,v.total_venta,v.impuesto,v.estado FROM venta v INNER JOIN persona p ON v.idcliente=p.idpersona INNER JOIN usuario u ON v.idusuario=u.idusuario WHERE DATE(v.fecha_hora)>='$fecha_inicio' AND DATE(v.fecha_hora)<='$fecha_fin' AND v.idcliente='$idcliente'";
+		return ejecutarConsulta($sql);		
+	}
+
+	public function totalpersonal()
+	{
+		$sql="SELECT IFNULL(SUM(modalidad),0) as totalp FROM personal WHERE DATE(fecha_hora)=curdate()";
+		return ejecutarConsulta($sql);
+	}
+
+	public function totalventahoy()
+	{
+		$sql="SELECT IFNULL(SUM(total_venta),0) as total_venta FROM venta WHERE DATE(fecha_hora)=curdate()";
+		return ejecutarConsulta($sql);
+	}
+
+	public function comprasultimos_10dias()
+	{
+		$sql="SELECT CONCAT(DAY(fecha_hora),'-',MONTH(fecha_hora)) as fecha,SUM(total_compra) as total FROM ingreso GROUP by fecha_hora ORDER BY fecha_hora DESC limit 0,10";
+		return ejecutarConsulta($sql);
+	}
+
+	public function ventasultimos_12meses()
+	{
+		$sql="SELECT DATE_FORMAT(fecha_hora,'%M') as fecha,SUM(total_venta) as total FROM venta GROUP by MONTH(fecha_hora) ORDER BY fecha_hora DESC limit 0,10";
+		return ejecutarConsulta($sql);
+	}
+	
+$date_nueva = date('Y-m-d');
+$date_inicio = date('Y-06-01');
+$date_fin = date('Y-06-31');
+ 
+if (verifica_rango($date_inicio, $date_fin, $date_nueva)) {
+    echo '<div class="alert alert-danger">Salimos de vacaciones.</div>';
+}
+else {
+    echo '<div class="alert alert-success">Atendemos de Lunes a viernes.</strong></div>';
+}
+
+}
+
+
+
+
+?>
